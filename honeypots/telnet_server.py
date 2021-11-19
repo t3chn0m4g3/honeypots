570803a580458b0559b3913e9a79086ca0ef4207
Marco Ochse <t3chn0m4g3@users.noreply.github.com>
2021-11-19 23:07:20 +0100

    JSON test

--- honeypots/telnet_server.py
+++ honeypots/telnet_server.py
@@ -11,6 +11,7 @@
 """
 
 from datetime import datetime
+from json import dumps
 from twisted.conch.telnet import TelnetProtocol, TelnetTransport
 from twisted.internet.protocol import Factory
 from twisted.internet import reactor
@@ -65,7 +66,7 @@ class QTelnetServer():
                 self._pass = None
                 self.transport.write(b'PC login: ')
                 self._state = b"Username"
-                _q_s.logs.info({"timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "protocol": "telnet", "action": "connection", "src_ip": self.transport.getPeer().host, "src_port": self.transport.getPeer().port, "dest_port": _q_s.port})
+                _q_s.logs.info(dumps({'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'protocol': 'telnet', 'action': 'connection', 'src_ip': self.transport.getPeer().host, 'src_port': self.transport.getPeer().port, 'dest_port': _q_s.port}))
             def dataReceived(self, data):
                 data = data.strip()
                 if self._state == b'Username':
@@ -77,9 +78,9 @@ class QTelnetServer():
                     self._user = self.check_bytes(self._user)
                     self._pass = self.check_bytes(self._pass)
                     if self._user == _q_s.username and self._pass == _q_s.password:
-                        _q_s.logs.info({"timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "protocol": "telnet", "action": "login", "status": "success", "src_ip": self.transport.getPeer().host, "src_port": self.transport.getPeer().port, "dest_port": _q_s.port, "username": _q_s.username, 'password': _q_s.password})
+                        _q_s.logs.info(dumps({'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'protocol': 'telnet', 'action': 'login', 'status': 'success', 'src_ip': self.transport.getPeer().host, 'src_port': self.transport.getPeer().port, 'dest_port': _q_s.port, 'username': _q_s.username, 'password': _q_s.password}))
                     else:
-                        _q_s.logs.info({"timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "protocol": "telnet", "action": "login", "status": "failed", "src_ip": self.transport.getPeer().host, "src_port": self.transport.getPeer().port, "dest_port": _q_s.port, "username": self._user, "password": self._pass})
+                        _q_s.logs.info(dumps({'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'protocol': 'telnet', 'action': 'login', 'status': 'failed', 'src_ip': self.transport.getPeer().host, 'src_port': self.transport.getPeer().port, 'dest_port': _q_s.port, 'username': self._user, 'password': self._pass}))
                     self.transport.loseConnection()
                 else:
                     self.transport.loseConnection()
@@ -102,17 +103,17 @@ class QTelnetServer():
                     self.port = port
                     self.process = Popen(['python3', path.realpath(__file__), '--custom', '--ip', str(self.ip), '--port', str(self.port), '--username', str(self.username), '--password', str(self.password), '--mocking', str(self.mocking), '--config', str(self.config), '--uuid', str(self.uuid)])
                     if self.process.poll() is None:
-                        self.logs.info({"timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "protocol": "telnet", "action": "process", "status": "success", "ip": self.ip, "port": self.port, "username": self.username, "password": self.password})
+                        self.logs.info(dumps({'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'protocol': 'telnet', 'action': 'process', 'status': 'success', 'ip': self.ip, 'port': self.port, 'username': self.username, 'password': self.password}))
                     else:
-                        self.logs.info({"timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "protocol": "telnet", "action": "process", "status": "error", "ip": self.ip, "port": self.port, "username": self.username, "password": self.password})
+                        self.logs.info(dumps({'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'protocol': 'telnet', 'action': 'process', 'status': 'error', 'ip': self.ip, 'port': self.port, 'username': self.username, 'password': self.password}))
                 else:
-                    self.logs.info({"timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "protocol": "telnet", "action": 'setup', "status": "error", "ip": self.ip, "port": self.port, "username": self.username, "password": self.password})
+                    self.logs.info(dumps({'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'protocol': 'telnet', 'action': 'setup', 'status': 'error', 'ip': self.ip, 'port': self.port, 'username': self.username, 'password': self.password}))
             elif self.close_port() and self.kill_server():
                 self.process = Popen(['python3', path.realpath(__file__), '--custom', '--ip', str(self.ip), '--port', str(self.port), '--username', str(self.username), '--password', str(self.password), '--mocking', str(self.mocking), '--config', str(self.config), '--uuid', str(self.uuid)])
                 if self.process.poll() is None:
-                    self.logs.info({"timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "protocol": "telnet", "action": "process", "status": "success", "ip": self.ip, "port": self.port, "username": self.username, "password": self.password})
+                    self.logs.info(dumps({'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'protocol': 'telnet', 'action': 'process', 'status': 'success', 'ip': self.ip, 'port': self.port, 'username': self.username, 'password': self.password}))
                 else:
-                    self.logs.info({"timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"), "protocol": "telnet", "action": "process", "status": "error", "ip": self.ip, "port": self.port, "username": self.username, "password": self.password})
+                    self.logs.info(dumps({'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ'), 'protocol': 'telnet', 'action': 'process', 'status': 'error', 'ip': self.ip, 'port': self.port, 'username': self.username, 'password': self.password}))
         else:
             self.telent_server_main()
 
